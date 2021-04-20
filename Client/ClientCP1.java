218a32bae4296e3f71385aa036b499d185454e25
Wah Kit <peeps.in.microwaves.explode@gmail.com>
2021-04-21 03:58:01 +0800

    initial commit

---
+++ Client/ClientCP1.java
@@ -0,0 +1,237 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Scanner;
+
+import javax.crypto.Cipher;
+
+public class ClientCP1 {
+
+	public static byte[] generateNonce() {
+		SecureRandom secureRandom = new SecureRandom();
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < 15; i++) {
+			stringBuilder.append(secureRandom.nextInt(10));
+		}
+		String randomNumber = stringBuilder.toString();
+		return randomNumber.getBytes();
+	}
+
+	public static void main(String[] args) {
+
+		String filename = null;
+
+    	String serverAddress = "localhost";
+    	if (args.length > 1) serverAddress = args[0];
+
+    	int port = 4321;
+    	if (args.length > 2) port = Integer.parseInt(args[1]);
+
+		int numBytes = 0;
+		byte[] encryptedNonce = null;
+		String certFileName = null;
+
+		Socket clientSocket = null;
+
+        DataOutputStream toServer = null;
+        DataInputStream fromServer = null;
+
+    	FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedFileInputStream = null;
+
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedFileOutputStream = null;
+
+		long timeStarted;
+
+		try {
+
+			System.out.println("Establishing connection to server...");
+
+			// Connect to server and get the input and output streams
+			clientSocket = new Socket(serverAddress, port);
+			toServer = new DataOutputStream(clientSocket.getOutputStream());
+			fromServer = new DataInputStream(clientSocket.getInputStream());
+
+			// Send nonce
+			System.out.println("Sending nonce...");
+			toServer.writeInt(0);
+			byte[] nonce = generateNonce();
+			toServer.writeInt(nonce.length);
+			toServer.write(nonce);
+			System.out.println("Sent nonce: " + new String(nonce, 0, nonce.length));
+
+			// wait for encrypted nonce
+			while (true) {
+				int packetType = fromServer.readInt();
+				if (packetType == 0) {
+					System.out.println("Receiving encrypted nonce...");
+					numBytes = fromServer.readInt();
+					encryptedNonce = new byte[numBytes];
+					fromServer.readFully(encryptedNonce, 0, numBytes);
+					break;
+				}
+			}
+
+			// Send request for certificate
+			System.out.println("Requesting for certificate... Spencer");
+			System.out.println(fromServer.available());
+			toServer.writeInt(1);
+
+			// wait for certificate
+			while (true) {
+
+				// int packetType = fromServer.readInt();
+				int packetType = fromServer.readInt();
+				System.out.println(packetType);
+				// Receiving certificate file name
+				if (packetType == 1) {
+					System.out.println("Receiving certificate file ...");
+					numBytes = fromServer.readInt();
+					byte[] certName = new byte[numBytes];
+					fromServer.readFully(certName, 0, numBytes);
+
+					certFileName = "recv_"+new String(certName, 0, numBytes);
+
+					fileOutputStream = new FileOutputStream(certFileName);
+					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+				}
+				// Receiving certificate file chunk
+				else if (packetType == 2) {
+
+					numBytes = fromServer.readInt();
+
+					byte [] block = new byte[numBytes];
+					fromServer.readFully(block, 0, numBytes);
+					System.out.println("numBytes");
+					System.out.println(numBytes);
+
+					if (numBytes > 0)
+						bufferedFileOutputStream.write(block, 0, numBytes);
+
+					if (numBytes < 117) {
+						if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+						if (bufferedFileOutputStream != null) fileOutputStream.close();
+						System.out.println("Certificate file received successfully");
+						break;
+					}
+				}
+			}
+
+			// Verify identity
+			System.out.println("Verifying identity...");
+
+			InputStream fis = new FileInputStream("cacsertificate.crt");
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			X509Certificate CAcert =(X509Certificate)cf.generateCertificate(fis);
+			PublicKey CAkey = CAcert.getPublicKey();
+
+			InputStream fis2 = new FileInputStream(certFileName);
+			X509Certificate serverCert =(X509Certificate)cf.generateCertificate(fis2);
+			
+			try {
+                serverCert.checkValidity();
+				serverCert.verify(CAkey);
+                System.out.println("Certificate verification success!");
+            } catch (Exception e) {
+                System.out.println("Certificate verification fail!");
+				return;
+            }
+
+			PublicKey serverPublicKey = serverCert.getPublicKey();
+
+			// Decrypt and verify nonce
+			System.out.println("Verifying nonce...");
+
+			Cipher decCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            decCipher.init(Cipher.DECRYPT_MODE, serverPublicKey);
+
+            byte[] decryptedNonce = decCipher.doFinal(encryptedNonce);
+			
+			if (decryptedNonce.equals(nonce)) {
+                System.out.println("Authentication success!");
+            }
+			else {
+                System.err.println("Authentification fail!");
+                System.err.println(nonce);
+                System.err.println(decryptedNonce);
+
+                toServer.close();
+                fromServer.close();
+                clientSocket.close();
+				return;
+            }
+
+			System.out.println("Ready to send file...");
+
+			// Loop and wait for user to input file names
+			while (true) {
+				System.out.println("Enter file name to upload, or enter 'quit' to quit:");
+				Scanner scanner = new Scanner(System.in);
+				
+				while (true) {
+					if (scanner.hasNextLine()) {
+                        filename = scanner.nextLine();
+						break;
+					}
+				}
+
+				// Quiting the session
+				if (filename.equals("quit")) {
+					System.out.println("Closing connection...");
+					toServer.writeInt(3);
+					toServer.close();
+					fromServer.close();
+					clientSocket.close();
+					scanner.close();
+					break;
+				}
+				
+				try {
+					timeStarted = System.nanoTime();
+					// Open the file
+					fileInputStream = new FileInputStream(filename);
+					bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+					toServer.writeInt(2);
+
+					// Send the filename
+					toServer.writeInt(0);
+					toServer.writeInt(filename.getBytes().length);
+					toServer.write(filename.getBytes());
+
+					byte [] fromFileBuffer = new byte[117];
+
+					// Send the file
+					for (boolean fileEnded = false; !fileEnded;) {
+						numBytes = bufferedFileInputStream.read(fromFileBuffer);
+						fileEnded = numBytes < 117;
+
+						toServer.writeInt(1);
+						toServer.writeInt(numBytes);
+						toServer.write(fromFileBuffer);
+						toServer.flush();
+					}
+
+					bufferedFileInputStream.close();
+	        		fileInputStream.close();
+
+					System.out.println("Successfully sent file: " + filename);
+					long timeTaken = System.nanoTime() - timeStarted;
+					System.out.println("File took: " + timeTaken/1000000.0 + "ms to send");
+
+				} catch (Exception e) {
+                    System.out.println("Invalid file name");
+				}
+			}
+		} catch (Exception e) {e.printStackTrace();}
+	}
+}
---
+++ Server/ServerCP1.java
@@ -0,0 +1,163 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.crypto.Cipher;
+import javax.sql.rowset.serial.SerialArray;
+
+
+public class ServerCP1 {
+
+	public static void main(String[] args) {
+
+    	int port = 4321;
+    	if (args.length > 0) port = Integer.parseInt(args[0]);
+
+		String certificateName = "server_cert.crt";
+
+		PrivateKey privateKey = null;
+		Cipher encCipher = null;
+		try {
+			byte[] keyBytes = Files.readAllBytes(Paths.get("private_key.der"));
+			PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			privateKey = kf.generatePrivate(spec);
+			encCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			encCipher.init(Cipher.ENCRYPT_MODE, privateKey);
+		} catch (Exception e) {e.printStackTrace();}
+
+		ServerSocket welcomeSocket = null;
+		Socket connectionSocket = null;
+		DataOutputStream toClient = null;
+		DataInputStream fromClient = null;
+
+		FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedFileInputStream = null;
+
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedFileOutputStream = null;
+
+		int numBytes = 0;
+
+		try {
+			welcomeSocket = new ServerSocket(port);
+			connectionSocket = welcomeSocket.accept();
+			fromClient = new DataInputStream(connectionSocket.getInputStream());
+			toClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			while (!connectionSocket.isClosed()) {
+
+				int packetType = fromClient.readInt();
+
+				switch (packetType) {
+				// packet for nonce and requesting for identity
+				case 0:
+					System.out.println("Receiving nonce and sending greeting...");
+					// receive nonce
+					numBytes = fromClient.readInt();
+					byte [] nonce = new byte[numBytes];
+					fromClient.readFully(nonce, 0, numBytes);
+					System.out.println("Received nonce: " + new String(nonce));
+
+					// encrypt nonce with private key and send to client
+					byte[] cipheredNonce = encCipher.doFinal(nonce);
+					System.out.println("Sending encrypted nonce to client..." );
+					toClient.writeInt(0);
+					toClient.writeInt(cipheredNonce.length);
+					toClient.write(cipheredNonce);
+
+					break;
+
+				// packet requesting for Certificate
+				case 1:
+					System.out.println("Received request for certificate, sending...");
+
+					// Send the certificate file name
+					toClient.writeInt(1);
+					toClient.writeInt(certificateName.getBytes().length);
+					toClient.write(certificateName.getBytes());
+
+					// Open the certificate file
+					fileInputStream = new FileInputStream(certificateName);
+					bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+
+					byte[] fromFileBuffer = new byte[117];
+
+					// Send the certificate file
+					for (boolean fileEnded = false; !fileEnded;) {
+						numBytes = bufferedFileInputStream.read(fromFileBuffer);
+						fileEnded = numBytes < 117;
+
+						toClient.writeInt(2);
+						toClient.writeInt(numBytes);
+						toClient.write(fromFileBuffer);
+						toClient.flush();
+					}
+
+					bufferedFileInputStream.close();
+					fileInputStream.close();
+					System.out.println("Certificate sent successfully");
+
+					break;
+					
+				// packet for transferring file
+				case 2:
+					while (true) {
+						int packetType2 = fromClient.readInt();
+						// packet for transferring file name
+						if (packetType2 == 0) {
+							numBytes = fromClient.readInt();
+							byte [] filename = new byte[numBytes];
+							// Must use read fully!
+							// See: https://stackoverflow.com/questions/25897627/datainputstream-read-vs-datainputstream-readfully
+							fromClient.readFully(filename, 0, numBytes);
+
+							fileOutputStream = new FileOutputStream("recv_" + new String(filename, 0, numBytes));
+							bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+							System.out.println("Received filename:" + new String(filename, 0, numBytes));
+						}
+						// packet for transferring a chunk of the file
+						else if (packetType2 == 1) {
+							numBytes = fromClient.readInt();
+							byte [] block = new byte[numBytes];
+							fromClient.readFully(block, 0, numBytes);
+
+							if (numBytes > 0)
+								bufferedFileOutputStream.write(block, 0, numBytes);
+
+							if (numBytes < 117) {
+								if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+								if (bufferedFileOutputStream != null) fileOutputStream.close();
+								System.out.println("File received successfully");
+								break;
+							}
+						}
+					}
+					break;
+
+				// packet for closing the session
+				case 3:
+					System.out.println("Closing connection...");
+					fromClient.close();
+					toClient.close();
+					connectionSocket.close();
+					break;
+
+				default:
+					break;
+				}
+			}
+		} catch (Exception e) {e.printStackTrace();}
+
+	}
+
+}
